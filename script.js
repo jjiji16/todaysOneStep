13cac301ffa1a36c33573a6f50b79d2d5508de7e
jjiji16 <djiyun16@gmail.com>
2025-09-26 10:45:40 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     // !!! 중요: README.md 파일을 읽고, 배포된 자신의 Google Apps Script 웹 앱 URL로 변경하세요.
-    const WEB_APP_URL = 'YOUR_GOOGLE_APPS_SCRIPT_URL_HERE';
+    const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbyFxk-9pG1KPaVy5DZ8Bm7GpVFxSPKQBvoQ913WltHmcE9nf8xWf3XiOTqqN4f9kreO/exec';
 
     const recordForm = document.getElementById('record-form');
     const recordsContainer = document.getElementById('records-container');
